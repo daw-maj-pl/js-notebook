3e77fef2645ffba1561109719d19ebcbbb379a9a
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2021-12-30 19:39:27 +0100

    Expand Resizer Horizontally

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import CodeEditor from './code-editor';
 import Preview from './preview';
 import bundle from '../bundler';
-import Resizable from './resizible';
+import Resizable from './resizable';
 
 const CodeCell = () => {
   const [code, setCode] = useState('');
@@ -15,14 +15,11 @@ const CodeCell = () => {
 
   return (
     <Resizable direction="vertical">
-      <div>
+      <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
         <CodeEditor
           initialValue="const a = 1;"
           onChange={value => setInput(value)}
         />
-        <div>
-          <button onClick={onClick}>Submit</button>
-        </div>
         <Preview code={code} />
       </div>
     </Resizable>
