ce917198396755a0514be6f2f01727d19c0e79a1
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-01-09 11:44:15 +0100

    Define Direction type

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -4,7 +4,8 @@ import {
   UpdateCellAction,
   DeleteCellAction,
   MoveCellAction,
-  InsertCellBeforeAction
+  InsertCellBeforeAction,
+  Direction
 } from '../actions';
 import { CellTypes } from '../cell';
 
@@ -25,10 +26,7 @@ export const deleteCell = (id: string): DeleteCellAction => {
   };
 };
 
-export const moveCell = (
-  id: string,
-  direction: 'up' | 'down'
-): MoveCellAction => {
+export const moveCell = (id: string, direction: Direction): MoveCellAction => {
   return {
     type: ActionType.MOVE_CELL,
     payload: {
