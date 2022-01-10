4ea7f75c90a6efd26d07ed56f0cde9a6f022bcf3
daw-maj-pl <71944368+daw-maj-pl@users.noreply.github.com>
2022-01-10 15:02:38 +0100

    Return state to satisfy TS

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -25,7 +25,7 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
       const { id, content } = action.payload;
 
       state.data[id].content = content;
-      return;
+      return state;
     // return {
     //   ...state,
     //   data: {
@@ -40,20 +40,20 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
       delete state.data[action.payload];
       state.order = state.order.filter(id => id !== action.payload);
 
-      return;
+      return state;
     case ActionType.MOVE_CELL:
       const { direction } = action.payload;
       const index = state.order.findIndex(id => id === action.payload.id);
       const targetIndex = direction === 'up' ? index - 1 : index + 1;
 
       if (targetIndex < 0 || targetIndex > state.order.length - 1) {
-        return;
+        return state;
       }
 
       state.order[index] = state.order[targetIndex];
       state.order[targetIndex] = action.payload.id;
 
-      return;
+      return state;
     case ActionType.INSERT_CELL_BEFORE:
       const cell: Cell = {
         content: '',
